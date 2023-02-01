825642ee75ed2d836df7f99740f498406392c77a
SiriFazendoBarra <manuel.martinez.perezdearce@gmail.com>
2023-01-31 21:46:55 -0300

    2nd, dislike btn off

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -10,17 +10,14 @@ export default function PhotoCard({ info }) {
     return (
         <div>
             <Card >
-{/* style={{ width: '20rem' }} */}
                 <Card.Img className='' variant="top" src={info.src?.medium} />
                 <Card.Body>
                     <Card.Text className='Card-Title h5'> {info.alt} - {info.photographer}</Card.Text>
                     <Card.Text className=''>{info.photographer_url}</Card.Text>
                     <div className='d-flex justify-content-between'>
                         <Button className='' variant="primary" onClick={() => addFavorites(info)}>Like</Button>
-                        <Button className='' variant="danger" onClick={() => addFavorites(info)}>Disslike</Button>
+                        <Button className='' variant="danger">Dislike</Button>
                     </div>
-
-
                 </Card.Body>
             </Card>
 
